3d9700bb2e73a14393d202f5b6cdb4f1155cf343
tanigawa <tanigawa@itpadminnoMacBook-Pro-3.local>
2021-06-26 16:44:51 +0900

    importとformの追加

--- src/main/java/work/upar/pokemon/entity/Pokemon.java
+++ src/main/java/work/upar/pokemon/entity/Pokemon.java
@@ -1,10 +1,6 @@
 package work.upar.pokemon.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/work/upar/pokemon/form/PokemonForm.java
+++ src/main/java/work/upar/pokemon/form/PokemonForm.java
@@ -1,2 +1,19 @@
-package work.upar.pokemon.form;public class PokemonForm {
+package work.upar.pokemon.form;
+
+import lombok.Data;
+
+@Data
+public class PokemonForm {
+  private Long id;
+  private String name;
+
+  private int h;
+  private int a;
+  private int b;
+  private int c;
+  private int d;
+  private int s;
+
+  private String type;
+  private String type2;
 }
